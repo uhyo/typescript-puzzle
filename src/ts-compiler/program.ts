52f3211064b105f2c0be275455c998b0f793db58
uhyo <uhyo@uhy.ooo>
2020-03-17 22:16:42 +0900

    remove unnecessary

--- src/ts-compiler/program.ts
+++ src/ts-compiler/program.ts
@@ -7,15 +7,12 @@ let sourceText: string = "";
 
 const host: ts.CompilerHost = {
   fileExists(fileName) {
-    console.log("fileExists", fileName);
-    return fileName === srcFileName;
+    return false;
   },
   readFile(fileName) {
-    console.log("readFile", fileName);
-    return sourceText;
+    return "";
   },
   getSourceFile(fileName, languageVersion) {
-    console.log("getSourceFile", fileName);
     if (fileName === srcFileName) {
       return ts.createSourceFile(fileName, sourceText, languageVersion);
     } else {
@@ -49,15 +46,12 @@ const host: ts.CompilerHost = {
  */
 export const getDiagnostics = (source: string) => {
   sourceText = source;
-  console.log("got sourceText:", source);
   program = ts.createProgram({
     rootNames: [srcFileName],
     options: {},
     host,
     oldProgram: program,
   });
-  console.log("program:", program);
   const diagnostics = ts.getPreEmitDiagnostics(program);
-  console.log(diagnostics);
   return diagnostics;
 };
