0913f23d2fb8bb24f298da0754f424408d27070e
uhyo <uhyo@uhy.ooo>
2020-03-23 01:55:32 +0900

    fix tracking

--- src/util/tracking/index.ts
+++ src/util/tracking/index.ts
@@ -1,4 +1,4 @@
-/// <reference types="google.analytics" />
+/// <reference types="gtag.js" />
 
 import { Level } from "~/definitions/stages/levels";
 
@@ -20,32 +20,26 @@ const eventAction = {
  * Track enter level event.
  */
 export const trackEnterLevel = (level: Level) => {
-  window.ga?.("send", {
-    hitType: "event",
-    eventCategory: eventCategory.level,
-    eventAction: eventAction.level.start,
-    eventLabel: String(level),
+  window.gtag?.("event", eventAction.level.start, {
+    event_category: eventCategory.level,
+    event_label: String(level),
   });
 };
 /**
  * Track clear level event.
  */
 export const trackClearLevel = (level: Level) => {
-  window.ga?.("send", {
-    hitType: "event",
-    eventCategory: eventCategory.level,
-    eventAction: eventAction.level.clear,
-    eventLabel: String(level),
+  window.gtag?.("event", eventAction.level.clear, {
+    event_category: eventCategory.level,
+    event_label: String(level),
   });
 };
 /**
  * Track stage clear event.
  */
 export const trackStageClear = (stageId: string) => {
-  window.ga?.("send", {
-    hitType: "event",
-    eventCategory: eventCategory.stage,
-    eventAction: eventAction.stage.clear,
-    eventLabel: stageId,
+  window.gtag?.("event", eventAction.stage.clear, {
+    event_category: eventCategory.stage,
+    event_label: stageId,
   });
 };
