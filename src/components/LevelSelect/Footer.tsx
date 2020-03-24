c10555ff2395e23042faf3df960462d308e3d8b9
uhyo <uhyo@uhy.ooo>
2020-03-25 01:22:32 +0900

    add footer help text

--- src/components/LevelSelect/Footer.tsx
+++ src/components/LevelSelect/Footer.tsx
@@ -5,18 +5,33 @@ import { grayTextColor, mainTextColor } from "~/design/color";
 export const Footer: FC = () => {
   return (
     <Wrapper>
-      <a href="/privacy.html">Privacy Policy</a>
-      <a target="_blank" href="https://github.com/uhyo/typescript-puzzle">
-        GitHub
-      </a>
+      <div>
+        <a href="/privacy.html">Privacy Policy</a>
+        <a target="_blank" href="https://github.com/uhyo/typescript-puzzle">
+          GitHub
+        </a>
+      </div>
+      <p>Help Type Puzzles by contributing questions!</p>
     </Wrapper>
   );
 };
 
 const Wrapper = styled.footer`
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: flex-start;
+  align-items: center;
+
   border-top: 1px solid ${mainTextColor};
   padding: 4px 0;
   font-size: 0.9rem;
+
+  p {
+    margin-top: 2px;
+    color: ${grayTextColor};
+    font-family: sans-serif;
+    font-size: 0.7rem;
+  }
 
   a {
     display: inline-block;
